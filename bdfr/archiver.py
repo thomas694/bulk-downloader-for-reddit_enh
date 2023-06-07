thomas694/bulk-downloader-for-reddit_with-saved-hashes

Add: Another list for storing the already downloaded URLs Add: Create backups of the hash files Add: New option '--save-hashes-interval' Add: Catch top-level exceptions and write back hashes

bdfr/archiver.py
@@ -40,31 +40,34 @@ class Archiver(RedditConnector):
         super(Archiver, self).__init__(args, logging_handlers)
 
     def download(self):
-        for generator in self.reddit_lists:
-            try:
-                for submission in generator:
-                    try:
-                        if (submission.author and submission.author.name in self.args.ignore_user) or (
-                            submission.author is None and "DELETED" in self.args.ignore_user
-                        ):
-                            logger.debug(
-                                f"Submission {submission.id} in {submission.subreddit.display_name} skipped due to"
-                                f" {submission.author.name if submission.author else 'DELETED'} being an ignored user"
-                            )
-                            continue
-                        if submission.id in self.excluded_submission_ids:
-                            logger.debug(f"Object {submission.id} in exclusion list, skipping")
-                            continue
-                        logger.debug(f"Attempting to archive submission {submission.id}")
-                        self.write_entry(submission)
-                    except (prawcore.PrawcoreException, praw.exceptions.PRAWException) as e:
-                        logger.error(f"Submission {submission.id} failed to be archived due to a PRAW exception: {e}")
-            except (prawcore.PrawcoreException, praw.exceptions.PRAWException) as e:
-                logger.error(f"The submission after {submission.id} failed to download due to a PRAW exception: {e}")
-                logger.debug("Waiting 60 seconds to continue")
-                sleep(60)
+        try:
+            for generator in self.reddit_lists:
+                try:
+                    for submission in generator:
+                        try:
+                            if (submission.author and submission.author.name in self.args.ignore_user) or (
+                                submission.author is None and "DELETED" in self.args.ignore_user
+                            ):
+                                logger.debug(
+                                    f"Submission {submission.id} in {submission.subreddit.display_name} skipped due to"
+                                    f" {submission.author.name if submission.author else 'DELETED'} being an ignored user"
+                                )
+                                continue
+                            if submission.id in self.excluded_submission_ids:
+                                logger.debug(f"Object {submission.id} in exclusion list, skipping")
+                                continue
+                            logger.debug(f"Attempting to archive submission {submission.id}")
+                            self.write_entry(submission)
+                        except (prawcore.PrawcoreException, praw.exceptions.PRAWException) as e:
+                            logger.error(f"Submission {submission.id} failed to be archived due to a PRAW exception: {e}")
+                except (prawcore.PrawcoreException, praw.exceptions.PRAWException) as e:
+                    logger.error(f"The submission after {submission.id} failed to download due to a PRAW exception: {e}")
+                    logger.debug("Waiting 60 seconds to continue")
+                    sleep(60)
+        except Exception as e:
+            logger.error(f"Uncaught exception: {e}")
         if self.args.keep_hashes:
-            self._hash_list_save()
+            self._hash_list_save(False)
 
     def get_submissions_from_link(self) -> list[list[praw.models.Submission]]:
         supplied_submissions = []
@@ -152,6 +155,7 @@ class Archiver(RedditConnector):
                     return
             self.master_hash_list[hash] = file_path
             self.master_file_list[str(file_path)] = hash
+            self._hash_list_save(True)
 
         file_path.parent.mkdir(exist_ok=True, parents=True)
         with Path(file_path).open(mode="w", encoding="utf-8") as file:

thomas694/bulk-downloader-for-reddit_enh

(cherry picked from commit 9f9ca16f7cbc9239bd9192d495520d4c308074c4)
(cherry picked from commit 45d54cd4716d5673adc9979a90fc423c493e6f45)

bdfr/archiver.py
@@ -41,33 +41,36 @@ class Archiver(RedditConnector):
         super(Archiver, self).__init__(args, logging_handlers)
 
     def download(self):
-        for generator in self.reddit_lists:
-            for chunk in batched(generator, 100):
-                try:
-                    submissions = self.load_submissions(chunk)
-                    for submission in submissions:
-                        try:
-                            if (submission.author and submission.author.name in self.args.ignore_user) or (
-                                submission.author is None and "DELETED" in self.args.ignore_user
-                            ):
-                                logger.debug(
-                                    f"Submission {submission.id} in {submission.subreddit.display_name} skipped due to"
-                                    f" {submission.author.name if submission.author else 'DELETED'} being an ignored user"
-                                )
-                                continue
-                            if submission.id in self.excluded_submission_ids:
-                                logger.debug(f"Object {submission.id} in exclusion list, skipping")
-                                continue
-                            logger.debug(f"Attempting to archive submission {submission.id}")
-                            self.write_entry(submission)
-                        except (prawcore.PrawcoreException, praw.exceptions.PRAWException) as e:
-                            logger.error(f"Submission {submission.id} failed to be archived due to a PRAW exception: {e}")
-                except (prawcore.PrawcoreException, praw.exceptions.PRAWException) as e:
-                    logger.error(f"The submission after {submission.id} failed to download due to a PRAW exception: {e}")
-                    logger.debug("Waiting 60 seconds to continue")
-                    sleep(60)
+        try:
+            for generator in self.reddit_lists:
+                for chunk in batched(generator, 100):
+                    try:
+                        submissions = self.load_submissions(chunk)
+                        for submission in submissions:
+                            try:
+                                if (submission.author and submission.author.name in self.args.ignore_user) or (
+                                    submission.author is None and "DELETED" in self.args.ignore_user
+                                ):
+                                    logger.debug(
+                                        f"Submission {submission.id} in {submission.subreddit.display_name} skipped due to"
+                                        f" {submission.author.name if submission.author else 'DELETED'} being an ignored user"
+                                    )
+                                    continue
+                                if submission.id in self.excluded_submission_ids:
+                                    logger.debug(f"Object {submission.id} in exclusion list, skipping")
+                                    continue
+                                logger.debug(f"Attempting to archive submission {submission.id}")
+                                self.write_entry(submission)
+                            except (prawcore.PrawcoreException, praw.exceptions.PRAWException) as e:
+                                logger.error(f"Submission {submission.id} failed to be archived due to a PRAW exception: {e}")
+                    except (prawcore.PrawcoreException, praw.exceptions.PRAWException) as e:
+                        logger.error(f"The submission after {submission.id} failed to download due to a PRAW exception: {e}")
+                        logger.debug("Waiting 60 seconds to continue")
+                        sleep(60)
+        except Exception as e:
+            logger.error(f"Uncaught exception: {e}")
         if self.args.keep_hashes:
-            self._hash_list_save()
+            self._hash_list_save(False)
 
     def get_submissions_from_link(self) -> list[list[praw.models.Submission]]:
         supplied_submissions = []
@@ -155,6 +158,7 @@ class Archiver(RedditConnector):
                     return
             self.master_hash_list[hash] = file_path
             self.master_file_list[str(file_path)] = hash
+            self._hash_list_save(True)
 
         file_path.parent.mkdir(exist_ok=True, parents=True)
         with Path(file_path).open(mode="w", encoding="utf-8") as file:
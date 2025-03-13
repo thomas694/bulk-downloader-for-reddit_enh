b2f5c9c05ccaf5bd39285bd7902ccbd1ec814a3f
Thomas <71355143+thomas694@users.noreply.github.com>
2025-03-13 23:05:39 +0100

    Fix issue Exception on empty list
    (cherry picked from commit b6f3920e903cec598df1047a48b604f06310f68e)

--- bdfr/cloner.py
+++ bdfr/cloner.py
@@ -25,6 +25,8 @@ class RedditCloner(RedditDownloader, Archiver):
                 for chunk in batched(generator, 100):
                     try:
                         submissions = self.load_submissions(chunk)
+                        if submissions is None:
+                            continue
                         for submission in submissions:
                             try:
                                 self._download_submission(submission)
